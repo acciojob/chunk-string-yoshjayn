74ac8bf0d84f81a3b915f0114554bff57d2023bf
Yashaswini Jain <122783078+yoshjayn@users.noreply.github.com>
2025-03-13 23:00:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function stringChop(str, size) {
   // your code here
+	    let i=0
+    while(i<str.length){
+        console.log('i',i)
+        arr.push(str.substring(i,i+size))
+        i+=size
+    }
+return arr
 }
 
 // Do not change the code below
